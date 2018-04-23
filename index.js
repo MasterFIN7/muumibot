8541861d9cae43a563523c27c2c08ca340c33c23
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-23 14:42:57 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+
+const PREFIX = "m!";
+
+var bot = new Discord.Client();
+
+var fortunes = [
+    "Yes",
+    "No",
+    "Maybe",
+    "Noob"
+];
+
+bot.on("ready", function() {
+    console.log("Ready");
+    bot.user.setGame("Use m!help");
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+  
+    if (!message.content.startsWith(PREFIX)) return;
+  
+    var args = message.content.substring(PREFIX.length).split(" ");
+  
+    switch (args[0].toLowerCase()) {
+      case "ping":
+        message.channel.sendMessage(`ping is ${Math.round(client.ping)}ms`);
+        break;
+      case "8ball":
+        if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+        else message.channel.sendMessage("Can't read that");
+        break;
+      case "help":
+        var embed = new Discord.RichEmbed()
+        .setTitle("My Prefix is m!")
+        .addField("Commands:", "ping\n8ball\nhelp")
+        .setColor("WHITE")
+        .setThumbnail(bot.user.avatarURL)
+        message.member.sendEmbed(embed);
+        
+        message.reply("Check your DM.")
+        break;
+      default:
+        message.channel.sendMessage("Invalid Command");
+    }
+});
+
+bot.login(process.env.BOT_TOKEN);
