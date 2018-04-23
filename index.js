4289992ff9d683eca1c21110d59cfb43e0650227
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-23 16:49:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ bot.on("message", function(message) {
   
     switch (args[0].toLowerCase()) {
       case "ping":
-        message.channel.sendMessage(`ping is ${Math.round(client.ping)}ms`);
+        message.channel.sendMessage(`ping is ${Math.round(bot.ping)}ms`);
         break;
       case "8ball":
         if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
