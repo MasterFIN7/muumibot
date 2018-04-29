3582dbc795b7f18b318709de5b414a6b9f8c7302
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 14:29:18 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,24 +3,25 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 
-  let hEmbed = new Discord.RichEmbed()
-  .setTitle("**Komennot**")
-  .setColor("#ffffff")
-  .Description: "**ban** Anna porttikielto käyttäjälle."
-  .Description: "**addrole** Lisää rooli käyttäjälle."
-  .Description: "**clear** Tyhjentää valitun määrän viestejä."
-  .Description: "**embed** Toistaa saman mitä sanot mutta eri muodossa."
-  .Description: "**help** Näyttää tämän valikon."
-  .Description: "**kick** Potkaise käyttäjä."
-  .Description: "**ping** Näyttää botin pingin."
-  .Description: "**removerole** Poistaa käyttäjän roolin."
-  .Description: "**report** Reporttaa käyttäjän."
-  .Description: "**say** Toistaa mitä sanot."
-  .Description: "**tempmute** Mutettaa käyttäjän haluamaksi ajaksi."
-  .Description: "**warn** Varoittaa käyttäjää."
-  .Description: "**warnlevel** Näyttää käyttäjän varoitukset.";
 
-  message.channel.send(hEmbed);
+
+  message.channel.send({embed: {
+    .setTitle("**Komennot**")
+    .setColor("#ffffff")
+    .Description: "**ban** Anna porttikielto käyttäjälle."
+    .Description: "**addrole** Lisää rooli käyttäjälle."
+    .Description: "**clear** Tyhjentää valitun määrän viestejä."
+    .Description: "**embed** Toistaa saman mitä sanot mutta eri muodossa."
+    .Description: "**help** Näyttää tämän valikon."
+    .Description: "**kick** Potkaise käyttäjä."
+    .Description: "**ping** Näyttää botin pingin."
+    .Description: "**removerole** Poistaa käyttäjän roolin."
+    .Description: "**report** Reporttaa käyttäjän."
+    .Description: "**say** Toistaa mitä sanot."
+    .Description: "**tempmute** Mutettaa käyttäjän haluamaksi ajaksi."
+    .Description: "**warn** Varoittaa käyttäjää."
+    .Description: "**warnlevel** Näyttää käyttäjän varoitukset."
+  }});
 
 }
 module.exports.help = {
