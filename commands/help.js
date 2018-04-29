740aa4921fb032221f31ee4fb2a780f78d51c1b4
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 14:21:54 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,19 +6,19 @@ module.exports.run = async (bot, message, args) => {
   let hEmbed = new Discord.RichEmbed()
   .setTitle("**Komennot**")
   .setColor("#ffffff")
-  .setDescription("**ban** Anna porttikielto käyttäjälle.")
-  .setDescription("**addrole** Lisää rooli käyttäjälle.")
-  .setDescription("**clear** Tyhjentää valitun määrän viestejä.")
-  .setDescription("**embed** Toistaa saman mitä sanot mutta eri muodossa.")
-  .setDescription("**help** Näyttää tämän valikon.")
-  .setDescription("**kick** Potkaise käyttäjä.")
-  .setDescription("**ping** Näyttää botin pingin.")
-  .setDescription("**removerole** Poistaa käyttäjän roolin.")
-  .setDescription("**report** Reporttaa käyttäjän.")
-  .setDescription("**say** Toistaa mitä sanot.")
-  .setDescription("**tempmute** Mutettaa käyttäjän haluamaksi ajaksi.")
-  .setDescription("**warn** Varoittaa käyttäjää.")
-  .setDescription("**warnlevel** Näyttää käyttäjän varoitukset.");
+  .Description("**ban** Anna porttikielto käyttäjälle.")
+  .Description("**addrole** Lisää rooli käyttäjälle.")
+  .Description("**clear** Tyhjentää valitun määrän viestejä.")
+  .Description("**embed** Toistaa saman mitä sanot mutta eri muodossa.")
+  .Description("**help** Näyttää tämän valikon.")
+  .Description("**kick** Potkaise käyttäjä.")
+  .Description("**ping** Näyttää botin pingin.")
+  .Description("**removerole** Poistaa käyttäjän roolin.")
+  .Description("**report** Reporttaa käyttäjän.")
+  .Description("**say** Toistaa mitä sanot.")
+  .Description("**tempmute** Mutettaa käyttäjän haluamaksi ajaksi.")
+  .Description("**warn** Varoittaa käyttäjää.")
+  .Description("**warnlevel** Näyttää käyttäjän varoitukset.");
 
   message.channel.send(hEmbed);
 
