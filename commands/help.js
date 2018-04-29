82c293961c82bad5b0f9e1152c6636481c5cb180
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 10:35:03 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,20 +6,21 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#ffffff")
   .setThumbnail(bot.user.avatarURL)
   .setTitle("**Komennot**")
-  .setDescription("")
-  .addField("**addrole**", "Lisää jollekkin rooli.")
-  .addField("**ban**", "Anna jäsenelle porttikielto.")
-  .addField("**clear**", "Tyhjennä valittu määrä viestejä.")
-  .addField("**embed**", "Toistaa mitä sanot eri muodossa.")
-  .addField("**help**", "Näyttää tämän listan.")
-  .addField("**kick**", "Potki jäsen.")
-  .addField("**ping**", "Näyttää botin Pingi.")
-  .addField("**removerole**", "Poista joku rooli.")
-  .addField("**report**", "Reporttaa käyttäjän.")
-  .addField("**say**", "Toistaa mitä sanot.")
-  .addField("**tempmute**", "Mykistä joku.")
-  .addField("**warn**", "Varoita jäsentä.")
-  .addField("**warnlevel**", "Näyttää jäsenen varoitukset");
+  .setDescription("Prefix on `m!`")
+
+  .addField("**addrole** Lisää jollekkin rooli.")
+  .addField("**ban** Anna jäsenelle porttikielto.")
+  .addField("**clear** Tyhjennä valittu määrä viestejä.")
+  .addField("**embed** Toistaa mitä sanot eri muodossa.")
+  .addField("**help** Näyttää tämän listan.")
+  .addField("**kick** Potki jäsen.")
+  .addField("**ping** Näyttää botin Pingi.")
+  .addField("**removerole** Poista joku rooli.")
+  .addField("**report** Reporttaa käyttäjän.")
+  .addField("**say** Toistaa mitä sanot.")
+  .addField("**tempmute** Mykistä joku.")
+  .addField("**warn** Varoita jäsentä.")
+  .addField("**warnlevel** Näyttää jäsenen varoitukset");
 
   message.channel.send(sEmbed);
 
