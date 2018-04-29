6f708a2923198e940c744ea64ab27536d057aaec
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 12:33:31 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   .setTitle("**Komennot**")
   .setColor("#ffffff")
   .setThumbnail(bot.user.avatarURL)
-  .addField("**ban**" "Anna porttikielto käyttäjälle.");
+  .addField("**ban**", "Anna porttikielto käyttäjälle.");
 
   message.channel.send(hEmbed);
 
