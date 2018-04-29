c3efdcf78458746275a96dc5c45c04b3529ed031
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 12:13:38 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+
+  fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+      console.log("En loydä komentoja!");
+      return;
+    }
+
+    jsfile.forEach((f, i) =>{
+      let props = require(`./commands/${f}`);
+      console.log(`${f} loaded!`);
+      bot.commands.set(props.help.name, props);
+    });
+  });
+
+  let hEmbed = new Discord.RichEmbed()
+  .setTitle("**Komennot:**")
+  .setDescription("Prefix on ``m!``")
+  .setColor("#ffffff")
+  .addBlakField(`${props}`);
+
+}
+module.exports.help = {
+  name: "help"
+}
