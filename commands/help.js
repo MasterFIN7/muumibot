f7c40890239f4d2be8bf77df03ff3997f9238479
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 10:31:49 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,21 @@ module.exports.run = async (bot, message, args) => {
   let sEmbed = new Discord.RichEmbed()
   .setColor("#ffffff")
   .setThumbnail(bot.user.avatarURL)
-  .addField("Komennot:", "addrole, ban, botinfo, clear, embed, help, kick, ping, removerole, report, say, serverinfo, tempmute, warn ja warnlevel");
+  .setTitle("**Komennot**")
+  .setDescription("")
+  .addField("**addrole**", "Lisää jollekkin rooli.")
+  .addField("**ban**", "Anna jäsenelle porttikielto.")
+  .addField("**clear**", "Tyhjennä valittu määrä viestejä.")
+  .addField("**embed**", "Toistaa mitä sanot eri muodossa.")
+  .addField("**help**", "Näyttää tämän listan.")
+  .addField("**kick**", "Potki jäsen.")
+  .addField("**ping**", "Näyttää botin Pingi.")
+  .addField("**removerole**", "Poista joku rooli.")
+  .addField("**report**", "Reporttaa käyttäjän.")
+  .addField("**say**", "Toistaa mitä sanot.")
+  .addField("**tempmute**", "Mykistä joku.")
+  .addField("**warn**", "Varoita jäsentä.")
+  .addField("**warnlevel**", "Näyttää jäsenen varoitukset");
 
   message.channel.send(sEmbed);
 
