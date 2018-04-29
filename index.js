163f6839faa04d5d5d38038e89a2a3a6ff498817
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 07:54:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,140 @@
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+let coins = require("./coins.json");
 
-const PREFIX = "m!";
+fs.readdir("./commands/", (err, files) => {
 
-var bot = new Discord.Client();
+  if(err) console.log(err);
 
-var fortunes = [
-    "Yes",
-    "No",
-    "Maybe",
-    "Noob"
-];
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("En loydä komentoja!");
+    return;
+  }
 
-bot.on("ready", function() {
-    console.log("Ready");
-    bot.user.setGame("Tee m!help");
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
 
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-  
-    if (!message.content.startsWith(PREFIX)) return;
-  
-    var args = message.content.substring(PREFIX.length).split(" ");
-  
-    switch (args[0].toLowerCase()) {
-      case "ping":
-        message.channel.sendMessage(`Pong! \`${client.pings[0]}ms\``);
-        break;
-      case "8ball":
-        if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-        else message.channel.sendMessage("Can't read that");
-        break;
-      case "kick":
-        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!kUser) return message.channel.send("En löydä käyttäjää!");
-        let kReason = args.join(" ").slice(22);
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`);
 
-        var kickEmbed = new Discord.RichEmbed()
-        .setDescription("~Kick~")
-        .addField("Kicked user", `${kUser} with ID ${kUser.id}`)
-        .addField("Kicked by", `<@${message.author.id}> with ID ${message.author.id}`)
-        .addField("Kicked In", message.channel)
-        .addField("Time", message.createdAt)
-        .addField("Reason", kReason);
+    bot.user.setActivity("V 1.0", {type: "PLAYING"});
 
-        let kickChannel = message.guild.channels.find(`name`, "incidents");
-        if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+    //bot.user.setGame("V 1.0");
+});
 
-        message.guild.member(kUser).kick(kReason);
-        kickChannel.send(kickEmbed);
-        break;
-      case "ban":
-        let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!bUser) return message.channel.send("En löydä käyttäjää!");
-        let bReason = args.join(" ").slice(22);
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-        if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
 
-        var banEmbed = new Discord.RichEmbed()
-        .setDescription("~Ban~")
-        .addField("Banned user", `${bUser} with ID ${bUser.id}`)
-        .addField("Banned by", `<@${message.author.id}> with ID ${message.author.id}`)
-        .addField("Banned In", message.channel)
-        .addField("Time", message.createdAt)
-        .addField("Reason", bReason);
+    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
-        let banChannel = message.guild.channels.find(`name`, "incidents");
-        if(!banChannel) return message.channel.send("Can't find incidents channel.");
-
-        message.guild.member(bUser).ban(bReason);
-        banChannel.send(banEmbed);
-        break;
-      case "say":
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Ei");
-        let botmessage = args.join(" ");
-        message.delete().catch();
-        message.channel.send(botmessage);
-        break;
-      case "help":
-        var embed = new Discord.RichEmbed()
-        .setTitle("My Prefix is m!")
-        .addField("Commands:", "ping\n8ball\nhelp")
-        .setColor("WHITE")
-        .setThumbnail(bot.user.avatarURL)
-        message.member.sendEmbed(embed);
-        
-        message.reply("Check your DM.")
-        break;
-      default:
-        message.channel.sendMessage("Invalid Command");
+    if(!prefixes[message.guild.id]){
+      prefixes[message.guild.id] = {
+        prefixes: botconfig.prefix
+      };
     }
+
+    let prefix = prefixes[message.guild.id].prefixes;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+
+    if(!coins[message.author.id]){
+      coins[message.author.id] = {
+        coins: 0
+      };
+    }
+
+    let coinAmt = Math.floor(Math.random() * 15) + 1;
+    let baseAmt = Math.floor(Math.random() * 15) + 1;
+    console.log(`${coinAmt} ; ${baseAmt}`);
+
+    if(coinAmt === baseAmt){
+      coins[message.author.id] = {
+        coins: coins[message.author.id].coins + coinAmt
+      };
+
+    fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+      if (err) console.log(err)
+    });
+    let coinEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setColor("#ffffff")
+    .addField("💰", `${coinAmt} Kolikkoja lisätty!`);
+
+    message.channel.send(coinEmbed);
+    }
+
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot,message,args);
+
+    // if(cmd === `${prefix}serverinfo`){
+    //
+    //     let sicon = message.guild.iconURL;
+    //     let sembed = new Discord.RichEmbed()
+    //     .setDescription("Server information")
+    //     .setColor("#ffffff")
+    //     .setThumbnail(sicon)
+    //     .addField("Server name", message.guild.name)
+    //     .addField("Created On", message.guild.createdAt)
+    //     .addField("You joined", message.member.joinedAt)
+    //     .addField("Total Members", message.guild.memberCount);
+    //
+    //     return message.channel.send(sembed);
+    // }
+    //
+    //
+    // if(cmd === `${prefix}report`){
+    //
+    //     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    //     if(!rUser) return message.channel.send("En löydä käyttäjää!");
+    //     let reason = args.join(" ").slice(22);
+    //
+    //     let reportEmbed = new Discord.RichEmbed()
+    //     .setDescription("Reports")
+    //     .setColor("#ffffff")
+    //     .addField("Reported user", `${rUser} with ID: ${rUser.id}`)
+    //     .addField("Reported by", `${message.author} with ID: ${message.author.id}`)
+    //     .addField("Channel", message.channel)
+    //     .addField("Time", message.createdAt)
+    //     .addField("Reason", reason);
+    //
+    //
+    //     let reportschannel = message.guild.channels.find(`name`, "reports");
+    //     if(!reportschannel) return message.channel.send("En löydä kanavaa!");
+    //
+    //
+    //     message.delete().catch(O_o=>{});
+    //     reportschannel.send(reportEmbed);
+    //
+    //     return;
+    // }
+    //
+    //
+    //
+    // if(cmd === `${prefix}botinfo`){
+    //
+    //     let bicon = bot.user.displayAvatarURL;
+    //     let botembed = new Discord.RichEmbed()
+    //     .setDescription("Infoa botista")
+    //     .setColor("#ffffff")
+    //     .setThumbnail(bicon)
+    //     .addField("Bot name", bot.user.username)
+    //     .addField("Created On", bot.user.createdAt);
+    //
+    //     return message.channel.send(botembed);
+    // }
 });
 
 bot.login(process.env.BOT_TOKEN);
