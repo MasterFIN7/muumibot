5830ef653e0b7a62f69e5f54384096ff25e0199b
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-29 14:07:17 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,20 @@ module.exports.run = async (bot, message, args) => {
   .setTitle("**Komennot**")
   .setColor("#ffffff")
   .setThumbnail(bot.user.avatarURL)
-  .addField("**ban**", "Anna porttikielto käyttäjälle.");
+  .addBlankField(true)
+  .addField("**ban**", "Anna porttikielto käyttäjälle.", true)
+  .addField("**addrole**", "Lisää rooli käyttäjälle.", true)
+  .addField("**clear**", "Tyhjentää valitun määrän viestejä.", true)
+  .addField("**embed**", "Toistaa saman mitä sanot mutta eri muodossa.", true)
+  .addField("**help**", "Näyttää tämän valikon.", true)
+  .addField("**kick**", "Potkaise käyttäjä.", true)
+  .addField("**ping**", "Näyttää botin pingin.", true)
+  .addField("**removerole**", "Poistaa käyttäjän roolin.", true)
+  .addField("**report**", "Reporttaa käyttäjän.", true)
+  .addField("**say**", "Toistaa mitä sanot.", true)
+  .addField("**tempmute**", "Mutettaa käyttäjän haluamaksi ajaksi.", true)
+  .addField("**warn**", "Varoittaa käyttäjää.", true)
+  .addField("**warnlevel**", "Näyttää käyttäjän varoitukset.", true);
 
   message.channel.send(hEmbed);
 
