479d3d99d1284a2ff9bd989799ffd15d3dc06799
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-30 09:20:44 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   let hEmbed = new Discord.RichEmbed()
-  .addBlankField(true)
   .addField("Test", "Hello", true);
 
   message.channel.send(hEmbed);
