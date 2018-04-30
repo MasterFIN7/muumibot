2d062aaa9f4a86b01f77412cba0906864fa0c15a
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-04-30 09:12:17 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let hEmbed = new Discord.RichEmbed()
+  .setTitle("Komennot")
+  .Title("Moi")
+
+}
+
+module.exports.help = {
+  name: "help"
+}
